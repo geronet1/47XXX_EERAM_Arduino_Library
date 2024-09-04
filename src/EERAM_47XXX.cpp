1e2a6fe027d380be19bac205edb2af545098037c
Jerry Magnin <dev@montagnisme.fr>
2024-09-04 18:49:05 +0200

    Added library source
    There is still a lot to do, mainly in error detection, but it should be functional (to be tested, though...)

---
+++ src/EERAM_47XXX.cpp
@@ -0,0 +1,153 @@
+#include "EERAM_47XXX.hpp"
+
+EERAM_47XXX::EERAM_47XXX(int address, IC_MEM_SIZE chipSize): m_address(address), m_chipSize(chipSize) {
+  // Config address can be derived from memory address. This makes process simpler for end user
+  m_config_address = m_address & ~(0b1111<<3) | (0b11<<3);
+}
+
+EERAM_47XXX::EERAM_47XXX(bool A1, bool A0, IC_MEM_SIZE chipSize): m_chipSize(chipSize) {
+  uint8_t addr_mask = 0;
+  if(A1) {
+    addr_mask |= 0b1 << 1;
+  }
+  if(A2) {
+    addr_mask |= 0b1 << 2;
+  }
+  m_config_address = 0b0011000 | addr_mask;
+  m_address = 0b1010000 | addr_mask;
+}
+
+void EERAM_47XXX::begin() {
+  Wire.begin();
+  m_wire_active = true;
+  getConfigStatus();
+}
+
+void EERAM_47XXX::writeConfigByte(int register_address, int payload) {
+  Wire.beginTransmission(m_config_address);
+  Wire.write(register_address);
+  Wire.write(payload);
+  Wire.endTransmission();
+}
+
+void EERAM_47XXX::writeStatus() {
+  writeConfigByte(REG_CONFIG_ADDRESS::STATUS, m_config_status_register);
+}
+
+int EERAM_47XXX::getConfigStatus() {
+  Wire.beginTransmission(m_config_address);
+  Wire.write(REG_CONFIG_ADDRESS::STATUS);
+  Wire.endTransmission();
+  Wire.requestFrom(m_config_address, 1);
+  m_config_status_register = Wire.read();
+  return(m_config_status_register);
+}
+
+bool EERAM_47XXX::setWriteProtect(STATUS_WRITE_PROTECT_BITS wp_mode) {
+  uint8_t wp_bits = wp_mode << 2;
+
+  if(!m_wire_active) {
+    begin();
+  }
+  
+  // Le ~ (NOT) met tous les autres bits à 1: on part de 0b00011100 à 0b11100011, en AND-ant on clear les bits.
+  m_config_status_register = m_config_status_register & ~(0b111<<2) | wp_bits;
+  writeStatus();
+}
+
+void EERAM_47XXX::removeWriteProtect() {
+  m_config_status_register = m_config_status_register & ~(0b111<<2);
+  writeStatus();
+}
+
+void EERAM_47XXX::enableAutoStore() {
+  m_config_status_register = m_config_status_register & ~(0b1<<1) | m_config_status_register;
+  writeStatus();
+}
+
+void EERAM_47XXX::disableAutoStore() {
+  m_config_status_register &= ~(0b1<<1);
+  writeStatus();
+}
+
+void EERAM_47XXX::resetEventDetector() {
+  m_config_status_register &= ~(0b1);
+  writeStatus();
+}
+
+bool EERAM_47XXX::eventDetected() {
+  getConfigStatus();
+  return(m_config_status_register & 1);
+}
+
+bool EERAM_47XXX::sramAltered(void) {
+  getConfigStatus();
+  return(m_config_status_register & 128); // 128 is 0b10000000
+}
+
+void EERAM_47XXX::memoryStore() {
+  writeConfigByte(REG_CONFIG_ADDRESS::COMMAND, COMMAND_EXEC::STORE);
+}
+
+void EERAM_47XXX::memoryRecall() {
+  writeConfigByte(REG_CONFIG_ADDRESS::COMMAND, COMMAND_EXEC::RECALL);
+}
+
+MEMORY_OP_STATUS EERAM_47XXX::write(int addressPointer, uint8_t singleByte) {
+  if(addressPointer < 0) {return(MEMORY_OP_STATUS::ADDRESS_DNE);}
+  if(addressPointer >= m_chipSize) {return(MEMORY_OP_STATUS::ADDRESS_OVER);}
+  Wire.beginTransmission(m_address);
+  Wire.write(addressPointer);
+  Wire.write(singleByte);
+  Wire.endTransmission();
+  return(MEMORY_OP_STATUS::WRITE_THEORY_OK);
+}
+
+MEMORY_OP_STATUS EERAM_47XXX::write(int addressPointer, uint8_t* dataArray, size_t arraySize, bool overwrite) {
+  if(addressPointer < 0) {return(MEMORY_OP_STATUS::ADDRESS_DNE);}
+  if(addressPointer >= m_chipSize) {return(MEMORY_OP_STATUS::ADDRESS_OVER);}
+  if(!overwrite && (addressPointer + arraySize) >= m_chipSize) {return(MEMORY_OP_STATUS::ADDRESS_ROLLOVER);}
+  Wire.beginTransmission(m_address);
+  Wire.write(addressPointer);
+  for(int i=0; i<arraySize; i++) {Wire.write(dataArray[i]);}
+  Wire.endTransmission();
+  return(MEMORY_OP_STATUS::WRITE_THEORY_OK);
+}
+
+uint8_t EERAM_47XXX::read() {
+  Wire.beginTransmission(m_address);
+  Wire.endTransmission();
+  Wire.requestFrom(m_address, 1);
+  return(Wire.read());
+}
+
+uint8_t EERAM_47XXX::read(int addressPointer){
+  if(addressPointer < 0) {return(MEMORY_OP_STATUS::ADDRESS_DNE);}
+  if(addressPointer >= m_chipSize) {return(MEMORY_OP_STATUS::ADDRESS_OVER);}
+
+  Wire.beginTransmission(m_address);
+  Wire.write(addressPointer);
+  Wire.endTransmission();
+  Wire.requestFrom(m_address, 1);
+  return(Wire.read());
+}
+
+/**
+* @brief Read an arbitrary number of bytes from memory
+* @warning Buffer should be at least the same size than byteCount, otherwise the program will crash
+*/
+MEMORY_OP_STATUS EERAM_47XXX::read(int addressPointer, uint8_t* buffer, size_t byteCount=1) {
+  if(addressPointer < 0) {return(MEMORY_OP_STATUS::ADDRESS_DNE);}
+  if(addressPointer >= m_chipSize) {return(MEMORY_OP_STATUS::ADDRESS_OVER);}
+
+  size_t i(0);
+  Wire.beginTransmission(m_address);
+  Wire.write(addressPointer);
+  Wire.endTransmission();
+  Wire.requestFrom(m_address, byteCount);
+  while(Wire.available()) {
+    buffer[i] = Wire.read();
+    i++;
+  }
+  return(MEMORY_OP_STATUS::READ_THEORY_OK);
+}
---
+++ src/EERAM_47XXX.hpp
@@ -0,0 +1,92 @@
+/**
+ * @file        EERAM_47XXX.hpp
+ * @brief       Header file for EERAM_47XXX Arduino Library
+ * @author      Jerry Magnin
+ * @version     1
+ * @date        2024
+ * @copyright   BSD 3-Clause
+ */
+
+
+#include "Arduino.h"
+#include <Wire.h>
+
+/**
+* @enum REG_CONFIG_ADDRESS
+* @brief Enumerates the existing registers for configuring the 47XXX Memory Chip
+*/
+typedef enum {
+  STATUS = 0x00, //!< Address of the \c STATUS register
+  COMMAND = 0x55 //!< \c COMMAND register, which can be used to either force store to or recall from EEPROM.
+} REG_CONFIG_ADDRESS;
+
+/**
+* @enum STATUS_WRITE_PROTECT_BITS
+* @brief Pre-defines values for \c STATUS register to allow software protection on memory ranges
+*/
+typedef enum {
+  UNPROTECTED = 0b000,
+  WP_UPPER_1_64 = 0b001,
+  WP_UPPER_1_32 = 0b010,
+  WP_UPPER_1_16 = 0b011,
+  WP_UPPER_1_8 = 0b100,
+  WP_UPPER_1_4 = 0b101,
+  WP_UPPER_1_2 = 0b110,
+  WP_ALL = 0b111
+} STATUS_WRITE_PROTECT_BITS;
+
+typedef enum {
+  STORE = 0b00110011,
+  RECALL = 0b11011101
+} COMMAND_EXEC;
+
+typedef enum {
+  C47X04 = 512,
+  C47X16 = 2048
+} IC_MEM_SIZE;
+
+typedef enum {
+  WRITE_THEORY_OK,
+  WRITE_OK,
+  READ_THEORY_OK,
+  ADDRESS_OVER,
+  ADDRESS_DNE,
+  ADDRESS_ROLLOVER,
+} MEMORY_OP_STATUS;
+
+class EERAM_47XXX {
+  public:
+    // Constructors & destructors
+    EERAM_47XXX(int address, IC_MEM_SIZE chipSize);
+    EERAM_47XXX(bool A1, bool A2, IC_MEM_SIZE chipSize);
+
+    // Initialisation 
+    void begin();
+
+    // Configuration-related functions
+    void writeConfigByte(int register_address, int payload);
+    void writeStatus();
+    int getConfigStatus(void);
+    bool setWriteProtect(STATUS_WRITE_PROTECT_BITS wp_mode);
+    void removeWriteProtect(void);
+    void enableAutoStore();
+    void disableAutoStore();
+    void resetEventDetector(void);
+    bool eventDetected(void);
+    bool sramAltered(void);
+    void memoryStore(void);
+    void memoryRecall(void);
+
+    // Memory storage related functions
+    MEMORY_OP_STATUS write(int addressPointer, uint8_t singleByte);
+    MEMORY_OP_STATUS write(int addressPointer, uint8_t* dataArray, size_t arraySize, bool overwrite);
+    uint8_t read();
+    uint8_t read(int addressPointer);
+    MEMORY_OP_STATUS read(int addressPointer, uint8_t* buffer, size_t byteCount);
+  private:
+    IC_MEM_SIZE m_chipSize;
+    int m_config_address;
+    int m_address;
+    bool m_wire_active;
+    int m_config_status_register;
+};
